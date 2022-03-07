bf1eb5123e4c3ef4d379dd22787775d6d01892fd
mon0macal7 <monhdz07@gmail.com>
2022-03-06 19:43:39 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+//import NavBar from "./components/NavBar/NavBar";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div>
+      <Button variant="text" color="secondary">
+        My name
+      </Button>
+      <Button variant="contained" color="secondary">
+        is Mono
+      </Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import "./NavBar.css";
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="name">
+        <h1>Prueba barra de navegación</h1>
+      </nav>
+    </div>
+  );
+};
+export default NavBar;
