883bf047a2101ea28f4375c9f0d50e6d56078ed5
mon0macal7 <monhdz07@gmail.com>
2022-06-02 23:38:38 -0500

    modify styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import React from "react";
+
 //import NavBar from "./components/NavBar/NavBar";
-import Button from "@mui/material/Button";
 
 function App() {
-  return (
-    <div>
-      <Button variant="text" color="secondary">
-        My name
-      </Button>
-      <Button variant="contained" color="secondary">
-        is Mono
-      </Button>
-    </div>
-  );
+  return <h1>Monofolio</h1>;
 }
 
 export default App;
